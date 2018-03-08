a3c7f3a98bcdf021ed112fabcf3146bca2a6747c
Kang-Li (Stephen) Cheng <11033065+kanglicheng@users.noreply.github.com>
2018-03-08 00:48:21 -0800

    Create areFollowingPatterns.py

---
+++ codeFights/Hash_Tables/areFollowingPatterns.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+def areFollowingPatterns(strings, patterns):
+    d1, d2 = defaultdict(list), defaultdict(list)
+    for i, e in enumerate(strings):
+        d1[e].append(i) 
+    for j, p in enumerate(patterns):
+        d2[p].append(j)
+    for i in range(len(strings)):
+        if d1[strings[i]] != d2[patterns[i]]:
+            return False
+    return True
